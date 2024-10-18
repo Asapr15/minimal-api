e437c2a8bfeb7137ca610606c6eaf71dd8e5191b
Asapr15 <alisson.asap@gmail.com>
2024-10-18 17:01:20 -0300

    Criação das Migrations

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using minimalApi.infraestrutura.Db;
+using minimalApi.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/Login", (minimalApi.DTOs.LoginDTO loginDTO) =>{
+app.MapPost("/Login", (LoginDTO loginDTO) =>{
 
     if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456"){
         return Results.Ok("Login com sucesso");
